f3759621aa0ffadcc798fba0af0607037918bc04
Rupak182 <rupakgoura18@gmail.com>
2025-12-23 18:15:34 +0530

    minor changes

--- app/page.tsx
+++ app/page.tsx
@@ -151,8 +151,8 @@ export default function Home() {
           </div>
 
           <div className="hidden md:flex items-center space-x-4">
-            <Button variant="ghost" className="lg:text-base text-sm bg-[linear-gradient(106.03deg,rgba(239,246,254,0.5)_0%,rgba(163,217,248,0.5)_238.47%)]  text-[rgba(108,132,255,1)] hover:opacity-70 border font-medium  border-[rgba(108,132,255,0.9)]">Sign in</Button>
-            <Button className="lg:text-base text-sm rounded-full bg-[linear-gradient(106.03deg,#677CFF_0%,#A3D9F8_238.47%)] hover:opacity-70  text-[rgba(238,246,251,1)]  font-medium ">
+            <Button variant="ghost" className="lg:text-base rounded-full text-sm bg-[linear-gradient(106.03deg,rgba(239,246,254,0.5)_0%,rgba(163,217,248,0.5)_238.47%)]  text-[rgba(108,132,255,1)] hover:opacity-70 border font-medium  border-[rgba(108,132,255,0.9)]">Sign in</Button>
+            <Button className="flex rounded-full items-center overflow-hidden p-5 lg:text-base text-sm  bg-[linear-gradient(106.03deg,#677CFF_0%,#A3D9F8_238.47%)] hover:opacity-70 text-[rgba(238,246,251,1)] font-medium ">
               Request a demo
             </Button>
           </div>
@@ -172,36 +172,56 @@ export default function Home() {
               onCloseAutoFocus={(e) => e.preventDefault()}
             >
               <nav className="flex flex-col gap-6 px-5 mt-8">
-                <Link
-                  href="#"
-                  className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
-                  onClick={() => setOpen(false)}
-                >
-                  About us
-                </Link>
-                <Link
-                  href="#"
-                  className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
-                  onClick={() => setOpen(false)}
-                >
-                  For Hiring Teams
-                </Link>
-                <Link
-                  href="#"
-                  className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
-                  onClick={() => setOpen(false)}
-                >
-                  For Job Seekers
-                </Link>
-                <Link
-                  href="#"
-                  className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
-                  onClick={() => setOpen(false)}
-                >
-                  Pricing
-                </Link>
+                <div className="flex items-center gap-4 border-[rgba(121,153,253,0.05)] border p-1 px-2 ">
+                  <Image src="/main/nav/about.svg" alt="Bait AI Logo" width={15} height={15} />
+                  <Link
+                    href="#"
+                    className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
+                    onClick={() => setOpen(false)}
+                  >
+                    About us
+                  </Link>
+                </div>
+
+                <div className="flex items-center gap-4 border-[rgba(121,153,253,0.05)] border p-1 px-2 ">
+                  <Image src="/main/nav/briefcase.svg" alt="briefcase" width={15} height={15} />
+                  <Link
+                    href="#"
+                    className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
+                    onClick={() => setOpen(false)}
+                  >
+                    For Hiring Teams
+                  </Link>
+                </div>
+
+                <div className="flex items-center gap-4 border-[rgba(121,153,253,0.05)] border p-1 px-2 ">
+                  <Image src="/main/nav/people.svg" alt="people" width={15} height={15} />
+
+                  <Link
+                    href="#"
+                    className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
+                    onClick={() => setOpen(false)}
+                  >
+                    For Job Seekers
+                  </Link>
+                </div>
+
+                <div className="flex items-center gap-4 border-[rgba(121,153,253,0.05)] border p-1 px-2 ">
+                  <Image src="/main/nav/money.svg" alt="money" width={15} height={15} />
+
+                  <Link
+                    href="#"
+                    className=" text-[rgba(10,13,26,0.7)] hover:opacity-70 font-medium py-2"
+                    onClick={() => setOpen(false)}
+                  >
+                    Pricing
+                  </Link>
+                </div>
+
                 <Separator className="my-2" />
-                <div className="flex flex-col gap-4 p-1">
+               
+              </nav>
+               <div className="flex flex-col gap-4 border-[rgba(121,153,253,0.05)] border  px-2 p-1">
                   <Button variant="ghost" className="w-full bg-[linear-gradient(106.03deg,rgba(239,246,254,0.5)_0%,rgba(163,217,248,0.5)_238.47%)] text-[rgba(108,132,255,1)] hover:opacity-70 border font-medium border-[rgba(108,132,255,0.9)]">
                     Sign in
                   </Button>
@@ -209,7 +229,6 @@ export default function Home() {
                     Request a demo
                   </Button>
                 </div>
-              </nav>
             </SheetContent>
           </Sheet>
         </nav>
@@ -444,7 +463,7 @@ export default function Home() {
 
 
           <div className="lg:text-4xl md:text-2xl text-base md:mb-4 mb-0 text-center text-[rgba(58,63,187,1)]">How it works</div>
-          
+
           <div className="md:w-xl w-[200px] mx-auto p-4 mb-2">
             <Separator className="bg-[rgba(201,231,81,0.7)]" />
           </div>
@@ -523,7 +542,7 @@ export default function Home() {
                 <Image className="md:size-6 lg:[size-30px] size-5" src="/main/facebook.svg" alt="Facebook Logo" width={30} height={30} />
               </div>
 
-              <div className="text-primary md:text-base lg:text-xl text-sm">
+              <div className="text-primary lg:text-base text-xs  md:text-sm">
                 Copyright © 2025 Bait AI | All Rights Reserved
               </div>
             </div>
