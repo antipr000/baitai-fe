aaa3a92e15bd97c7b6c7c4e09df5b893c18683a9
Rupak182 <rupakgoura18@gmail.com>
2025-12-29 20:25:36 +0530

    fixes

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 "use client";
-import React, { useEffect, useState } from "react";
-import { Footer } from "@/components/footer";
+import  { useEffect, useState } from "react";
 
 function getTimeLeft(targetDate: Date) {
   const now = new Date();
