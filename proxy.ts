2e912f92f193c0777bd98f9c5e3c36ae9d925def
Rupak182 <rupakgoura18@gmail.com>
2026-01-12 00:27:15 +0530

    added edge flag

--- proxy.ts
+++ proxy.ts
@@ -5,6 +5,8 @@ import { clientConfig, serverConfig } from "@/lib/auth/config";
 const AUTH_PAGES = ['/signup', '/login'];
 const PUBLIC_PATHS = ['/', '/about', '/pricing', ...AUTH_PAGES];
 
+export const runtime = "edge";
+
 export async function proxy(request: NextRequest) {
 
   return authMiddleware(request, {
