26dee0997cdf72da7a2657fc62469e5d8f415a61
Rupak Kumar Goura <rupakgoura18@gmail.com>
2026-02-18 00:23:20 +0530

    posthog testing

--- instrumentation-client.ts
+++ instrumentation-client.ts
@@ -1,35 +1,26 @@
-'use client'
 import posthog from 'posthog-js'
 
-if (typeof window !== 'undefined') {
-  // We define the logic
-  const initPostHog = () => {
-    const EU_COUNTRY_CODES = [
-      'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR',
-      'DE', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL',
-      'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB', 'CH', 'NO', 'IS', 'LI'
-    ];
+// Check if the user is in the EU based on the meta tag injected by layout
+const EU_COUNTRY_CODES = [
+  'AT', 'BE', 'BG', 'HR', 'CY', 'CZ', 'DK', 'EE', 'FI', 'FR',
+  'DE', 'GR', 'HU', 'IE', 'IT', 'LV', 'LT', 'LU', 'MT', 'NL',
+  'PL', 'PT', 'RO', 'SK', 'SI', 'ES', 'SE', 'GB', 'CH', 'NO', 'IS', 'LI'
+];
 
-    const countryMeta = document.querySelector('meta[name="geo-country"]');
-    const countryCode = countryMeta?.getAttribute('content')?.toUpperCase() || 'NAN';
-    const isEUUser = EU_COUNTRY_CODES.includes(countryCode);
-
-    if (process.env.NEXT_PUBLIC_ENV==="production" && !isEUUser) {
-      posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
-        api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
-        person_profiles: 'identified_only',
-        session_recording: { maskAllInputs: true },
-      });
-      console.log("PostHog initialized for:", countryCode);
-    }
-  };
-
-  // If the DOM is already ready, run it.
-  // Otherwise, wait for 'DOMContentLoaded'
-  if (document.readyState === 'loading') {
-    document.addEventListener('DOMContentLoaded', initPostHog);
-  } else {
-    initPostHog();
-  }
+let isEUUser = false;
+if (typeof document !== 'undefined') {
+  const countryMeta = document.querySelector('meta[name="geo-country"]');
+  const countryCode = countryMeta ? countryMeta.getAttribute('content') : 'NAN';
+  isEUUser = EU_COUNTRY_CODES.includes(countryCode?.toUpperCase() || '');
 }
 
+if (process.env.NEXT_PUBLIC_ENV === 'production' && !isEUUser) {
+  posthog.init(process.env.NEXT_PUBLIC_POSTHOG_KEY!, {
+    api_host: process.env.NEXT_PUBLIC_POSTHOG_HOST || 'https://us.i.posthog.com',
+    person_profiles: 'identified_only',
+    session_recording: {
+      maskAllInputs: true,
+      // maskTextSelector: "*", // GDPR: Mask all text
+    },
+  });
+}
